6ae40b384cd3345b1a87a4f8a909a659e15bbcea
OlegBiletsky <ol.biletsky@gmail.com>
2020-09-22 15:26:21 +0300

    1-3

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 //1. Напишіть функцію, що повертає куб числа.
+/*
+
 alert('task1');
 //******************************************
 function getCube(x) {
@@ -12,27 +14,182 @@ let getKube = function (x) {
 }
 console.log(getKube(5));
 //******************************************
-let getCubeOfNumber = (x) => {
-    return (x ** 3)
-}
+let getCubeOfNumber = (x) => x ** 3;
 console.log(getCubeOfNumber(5));
 
 
 
 
-/*
-2. Напишіть функцію, що додає перше число до другого і ділить результат на третє число.
-3. Напишіть функцію, що приймає число від 1 до 7 і повертає відповідну назву дня (українською).
-4. Реалізуйте функцію знаходження факторіала
-5. Напишіть функцію яка отримує години хвилини та секунди і повертає це число в секундах.
-6. Написати функцію , яка приймає секунди, і перетворює їх у години хвилини та секунди у форматі «гг:хх:сс». якщо кількість годин більша за 23.59.59 - вивести повідомлення "Більше одного дня". 
+
+
+
+
+
+
+
+
+
+
+//2. Напишіть функцію, що додає перше число до другого і ділить результат на третє число.
+alert('task2');
+function actionWithNumbers (a, b, c) {
+    return ( ( a + b ) / c );
+}
+console.log(actionWithNumbers(1,2,3));
+//******************************************
+let action2 = function (a, b, c) {
+    let result = ( a + b) / c;
+    return result;
+}
+console.log(action2(1,2,3));
+//******************************************
+let action3 = (a, b, c) => { return ((a+b)/c); };
+console.log(action3(1,2,3));
+
+
+
+
+
+
+
+
+
+
+
+
+
+//3. Напишіть функцію, що приймає число від 1 до 7 і повертає відповідну назву дня.
+alert('task3');
+
+
+function getDay (numberOfDay) {
+    switch (numberOfDay) {
+        case 1 : return "monday";
+        case 2 : return "tuesday";
+        case 3 : return "wednesday";
+        case 4 : return "thursday";
+        case 5 : return "friday";
+        case 6 : return "saturday";
+        case 7 : return "sunday";
+        default : return "Неправильне число";
+    }
+}
+console.log(getDay(6));
+//******************************************
+let getDay2 = function (numberOfDay) {
+    switch (numberOfDay) {
+        case 1 : return "monday";
+        case 2 : return "tuesday";
+        case 3 : return "wednesday";
+        case 4 : return "thursday";
+        case 5 : return "friday";
+        case 6 : return "saturday";
+        case 7 : return "sunday";
+        default : return "Неправильне число";
+    }
+}
+console.log(getDay2(7));
+//******************************************
+let getDay3 = (numberOfDay) => {
+    switch (numberOfDay) {
+        case 1 : return "monday";
+        case 2 : return "tuesday";
+        case 3 : return "wednesday";
+        case 4 : return "thursday";
+        case 5 : return "friday";
+        case 6 : return "saturday";
+        case 7 : return "sunday";
+        default : return "Неправильне число";
+    }
+}
+console.log(getDay3(8));
+
+//******************************************
+week = {
+    1: {day: "Monday"},
+    2: {day: "Tuesday"},
+    3: {day: "Wednesday"},
+    4: {day: "Thursday"},
+    5: {day: "Friday"},
+    6: {day: "Saturday"},
+    7: {day: "Sunday"},
+}
+
+//obj['day']
+
+let getDay4 = (numberDayOfWeek) => {
+        const obj = week[numberDayOfWeek];
+        return obj ? obj.day : 'неправильний номер'
+    }
+alert(getDay4(3));
+
+
+
+
+
+
+
+
+
 */
+
+//4. Реалізуйте функцію знаходження факторіала
+alert('task4');
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+5. Напишіть функцію яка отримує години хвилини та секунди і повертає це число в секундах.
+alert('task5');
+
+
+
+
+
+
+
+
+
+6. Написати функцію , яка приймає секунди, і перетворює їх у години хвилини та секунди у форматі «гг:хх:сс». якщо кількість годин більша за 23.59.59 - вивести повідомлення "Більше одного дня".
+alert('task6');
+*/
+
+
+
+
+
+
+
+
+
+
 //7. 4 відмінності ерров фанкшина від звичайної функції.
-//коротший спосіб написання синтаксису, коли лінь писати багато коду))
-//у стрілкових функцій немає this, якщо потрібно - його значення береться зовні.?
-//стрелочные функции не могут быть использованы как конструкторы. Они не могут быть вызваны с new.?
-//У стрелочных функций также нет переменной arguments.?
-//у них также нет super.?
+//-коротший спосіб написання синтаксису, коли лінь писати багато коду))
+//-у стрілкових функцій немає this, якщо потрібно - його значення береться зовні.-?
+//-стрелочные функции не могут быть использованы как конструкторы. Они не могут быть вызваны с new-?
+//-У стрелочных функций также нет переменной arguments.-?
+//-у них также нет super.-?
+
+
+
+
+
+
+
+
+
+
+
 
 
 /*
